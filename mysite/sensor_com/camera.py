3f1df189fb6546ad2b348778d7a2d2e62261ac6c
Martin Loeland <martinloland@gmail.com>
2018-01-29 16:03:05 +0100

    fix to camera

--- mysite/sensor_com/camera.py
+++ mysite/sensor_com/camera.py
@@ -2,6 +2,7 @@ import platform, os
 from time import sleep
 from datetime import datetime
 if platform.system() is not 'Windows':
+    print('importing picamera...')
     from picamera import PiCamera
 
 
@@ -9,7 +10,7 @@ class Camera(object):
     def __init__(self, debug, resolution):
         self.debug = False
         self.folder_url = '../media/camera/'
-        self.folder_save = '../static/media/camera/'
+        self.folder_save = '../media/camera/'
         self.suffix = '.jpg'
         if not debug:
             self.picam = PiCamera()
@@ -17,27 +18,29 @@ class Camera(object):
 
     def capture(self, capture_at_night, light_on):
         if self._decide_to_capture(capture_at_night, light_on):
-            if not self.debug:
-                filename = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-                base = os.path.dirname(__file__)
-                path_url = os.path.join(self.folder_url, filename+self.suffix)
-                path_save = os.path.join(base, self.folder_save+filename+self.suffix)
-                self.picam.start_preview()
-                sleep(2)
-                # self.picam.capture(path_save)
-                self.picam.capture('/home/pi/greenhouse/mysite/media/camera/flower.jpg')
-                # return path_url
-
-        return self._default_image()
+            filename = str(int(datetime.now().timestamp()))
+            base = os.path.dirname(__file__)
+            path_url = os.path.join(self.folder_url, filename+self.suffix)
+            path_save = os.path.join(base, self.folder_save+filename+self.suffix)
+            path_save_latest = os.path.join(base, self.folder_save+'latest'+self.suffix)
+            self.picam.start_preview()
+            sleep(2)
+            self.picam.capture(path_save)
+            print('saved to {}'.format(path_save))
+            self.picam.capture(path_save_latest)
+            print('saved to {}'.format(path_save_latest))
+            return path_url
+        else:
+            return self._default_image()
 
     def _decide_to_capture(self, capture_at_night, light_on):
-        # if light_on:
-        #     return True
-        # elif capture_at_night:
-        #     return True
-        # else:
-        #     return False
-        return True
+        if not self.debug:
+            if light_on or capture_at_night:
+                return True
+            else:
+                return False
+        else:
+            return False
 
     def _default_image(self):
         filename = 'flower'
@@ -45,8 +48,4 @@ class Camera(object):
         path_url = os.path.join(self.folder_url, filename + self.suffix)
         path_save = os.path.join(base,
                                  self.folder_save + filename + self.suffix)
-        # file = path_save+'delete.txt'
-        # with open(file, 'w') as f:
-        #     f.write("hey")
-        # print('created '+file)
         return path_url
